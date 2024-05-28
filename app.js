a12f8a4afed941ecd503b0451711ec24a11a5f8f
smburke1 <78122729+smburke1@users.noreply.github.com>
2024-05-28 13:26:12 -0700

    Resetting

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ prevBtn.addEventListener('click', function() {
     
    });
    function myFunction() {
-    var x = document.getElementById("myTopnav");
+    var x = document.getElementById("myTopNav");
     if (x.className === "topnav") {
       x.className += " responsive";
     } else {
